96a6db9d4c924a866bd00bce18d964598d4e3818
Bocmen <den20025plus@gmail.com>
2021-03-07 12:17:32 +0300

    ?

---
+++ Exemple/FilmsAndSerialCrossPlatform/FilmsAndSerialCrossPlatform/FilmsAndSerialCrossPlatform/Pages/SearchPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FilmsAndSerialCrossPlatform.Pages
+{
+    public class SearchPage : ContentPage
+    {
+
+        public SearchPage()
+        {
+            Button buttonT = new Button { Text = "NewPage" };
+            buttonT.Clicked += ButtonT_Clicked;
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" },
+                    buttonT
+                }
+            };
+        }
+
+        private void ButtonT_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SearchPage());
+        }
+    }
+}
